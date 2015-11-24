5df4d4082a56bec37f958a528f00544218055468
Ahmad Adnan Kaifi <kaifi@live.com>
2015-11-24 19:06:15 +0300

    fix brand and nav bottom

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -13,7 +13,7 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-fixed-top' ),
 		didScroll = false,
-		changeHeaderOn = 300;
+		changeHeaderOn = 120;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
