6c2c8252a4c49811f64fabe80b33cf73797d96ab
Ahmad Adnan Kaifi <kaifi@live.com>
2015-11-29 02:01:02 +0300

    Fix drawing error

--- multipleObjectTracking.cpp
+++ multipleObjectTracking.cpp
@@ -99,6 +99,16 @@ void drawObject(vector<Object> theObjects,Mat &frame, Mat &temp, vector< vector<
 	}
 }
 
+void drawObject(vector<Object> theObjects,Mat &frame){
+
+	for(int i =0; i<theObjects.size(); i++){
+
+	cv::circle(frame,cv::Point(theObjects.at(i).getXPos(),theObjects.at(i).getYPos()),10,cv::Scalar(0,0,255));
+	cv::putText(frame,intToString(theObjects.at(i).getXPos())+ " , " + intToString(theObjects.at(i).getYPos()),cv::Point(theObjects.at(i).getXPos(),theObjects.at(i).getYPos()+20),1,1,Scalar(0,255,0));
+	cv::putText(frame,theObjects.at(i).getType(),cv::Point(theObjects.at(i).getXPos(),theObjects.at(i).getYPos()-30),1,2,theObjects.at(i).getColour());
+	}
+}
+
 void morphOps(Mat &thresh){
 
 	//create structuring element that will be used to "dilate" and "erode" image.
